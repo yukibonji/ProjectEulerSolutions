0d649a917bd28350e7341997345823e53065000c
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-07-07 06:47:38 +0200

    Added a solution for Problem 1

--- ProjectEulerSolutions/Problem001.fs
+++ ProjectEulerSolutions/Problem001.fs
@@ -1,7 +1,11 @@
 namespace Iit.Fsharp.ProjectEulerSolutions
+open System.Linq
 
 type Problem001() =
-    let result () = 2
+    let numbersDividableBy3or5 input = input |> Seq.filter (fun item -> (item % 3 * item % 5) = 0)
+    let solution input = numbersDividableBy3or5 input |> Seq.sum
+    
+    let result maxValue () = solution (System.Linq.Enumerable.Range (1, maxValue))
     interface IProblemSolution with
         member x.ProblemId = 1
-        member x.SolutionAlgorithm with get () = result
+        member x.SolutionAlgorithm with get () = result 999
