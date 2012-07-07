2beb4c16ffbe07e3913278685cb43f434b29cad2
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-07-07 23:13:09 +0200

    Added a solution for the problem 2

---
+++ ProjectEulerSolutions/Problem002.fs
@@ -0,0 +1,28 @@
+namespace Iit.Fsharp.ProjectEulerSolutions
+open System.Linq
+
+type Problem002() =
+    let rec fib n =
+        match n with
+            | 0 -> 1
+            | 1 -> 2
+            | n -> fib (n-1) + fib (n-2)
+    
+    let fibUntil maxValue =
+        let rec generate i =
+            let fibValue = fib i
+            match fibValue with
+                | x when x <= maxValue -> x :: generate (i+1)
+                | _ -> []
+        generate 0
+    
+    let even x = (x % 2) = 0
+    let result () =
+        raise (System.Exception())
+        fibUntil 4000000
+        |> List.filter even
+        |> List.sum
+
+    interface IProblemSolution with
+        member x.ProblemId = 2
+        member x.SolutionAlgorithm with get () = result
