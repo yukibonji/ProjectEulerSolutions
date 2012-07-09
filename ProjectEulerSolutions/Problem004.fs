b83527a45f77d7973f8a84726aa9abb2c3040ce1
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-07-09 22:03:41 +0200

    Added a solution for problem 4

---
+++ ProjectEulerSolutions/Problem004.fs
@@ -0,0 +1,22 @@
+namespace Iit.Fsharp.ProjectEulerSolutions
+open System.Linq
+
+type Problem004() =
+    let isSymmetric sequence =
+        let orig = List.ofSeq sequence
+        let reversed = List.rev orig
+        orig = reversed
+        
+    let isPalindrome number = List.ofSeq (number.ToString()) |> isSymmetric
+
+    let largestPalindrome =
+        let left = [999..-1..100]
+        let right = [999..-1..100]
+        let findLargestPalindrome n = List.tryFind isPalindrome (List.map (fun x -> n * x) right)
+        List.map findLargestPalindrome left |> List.max |> Option.get
+
+    let result () = largestPalindrome
+
+    interface IProblemSolution with
+        member x.ProblemId = 5
+        member x.SolutionAlgorithm with get () = result
