6e2adfb22ada1d65343b24adc9389ac414a8ff9a
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-07-09 05:02:47 +0200

    checking in wip

--- ProjectEulerSolutions/EratosthenesEager.fs
+++ ProjectEulerSolutions/EratosthenesEager.fs
@@ -1,16 +1,23 @@
 namespace Iit.Fsharp.ProjectEulerSolutions
 
 type EratosthenesEager(number: int64) =
-    let rec sieve list = 
-        let relPrime divisor dividend = not ((dividend % divisor) = 0)
-        match list with
-            | head::tail -> head :: (sieve (tail |> List.filter (relPrime head)))
-            | [] -> []
+
+    let sieve maxPrime = 
+        let (list : bool[]) = Array.create (maxPrime + 1) true
+        let markOff number = [2*number..number..list.Length] |> List.iter (fun index -> list.[index] <- false)
+        let rec sieveNext number =
+            if number >= list.Length then
+                ()
+            if list.[number] = true then
+                markOff number
+            sieveNext (number + 1)
+        sieveNext 2
+        list
 
     let maxPrime number = sqrt (float number) |> System.Math.Ceiling |> int
 
     let sieveFor number =
-        sieve [2..(maxPrime number)]
+        sieve (maxPrime number)
 
     interface IPrimeNumberProvider with
         member x.Primes with get () = seq (sieveFor number)
