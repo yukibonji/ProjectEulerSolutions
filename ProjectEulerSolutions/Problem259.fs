301c347c4067aa9520bc2e6fc58279c6fa5dd0e4
DanielFabian <daniel.fabian@integral-it.ch>
2012-07-10 11:35:35 +0200

    added a skeleton for Problem 259

---
+++ ProjectEulerSolutions/Problem259.fs
@@ -0,0 +1,30 @@
+﻿namespace Iit.Fsharp.ProjectEulerSolutions
+open System.Linq
+
+type Problem259() =
+    /// enumerates partitions of a list of non-empty left and right parts
+    let rec partitions list =
+        let attach head (left, right) = (head::left, right)
+        seq {
+            match list with
+                | [a] -> ()
+                | [] -> ()
+                | [a;b] -> yield ([a], [b])
+                | h::t -> 
+                    yield ([h], t)
+                    yield! partitions t |> Seq.map (attach h)
+        }
+
+    /// creates a number from a list of digits
+    let number list =
+        let rec cat n list =
+            match list with
+                | [] -> n
+                | h::t -> cat (10 * n + h) t
+        cat 0 list
+
+    let result () = 123
+
+    interface IProblemSolution with
+        member x.ProblemId = 5
+        member x.SolutionAlgorithm with get () = result
