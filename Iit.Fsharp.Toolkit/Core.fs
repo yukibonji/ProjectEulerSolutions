1998989d331baefee0792390db2e3feac607eb2a
DanielFabian <daniel.fabian@integral-it.ch>
2012-07-10 20:38:24 +0200

    Added IIT F# toolkit

---
+++ Iit.Fsharp.Toolkit/Core.fs
@@ -0,0 +1,16 @@
+﻿namespace Iit.Fsharp.Toolkit
+
+open System.Collections.Generic
+open System.Linq
+module Core =
+
+    let cache f =
+        let valueCache = new Dictionary<_,_>()
+        fun x ->
+            let ok, result = valueCache.TryGetValue(x)
+            if ok then
+                result
+            else
+                let result = f x
+                valueCache.[x] <- result
+                result
