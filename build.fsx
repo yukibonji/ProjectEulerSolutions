88537b2e1cd303b22f48b7f72c604485bd2b695e
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-07-15 16:54:03 +0200

    added a dummy build step with clean as a dependency.

--- build.fsx
+++ build.fsx
@@ -1,10 +1,21 @@
 // include Fake libs
 #I @"lib/FAKE-1.64.6.0"
 #r "FakeLib.dll"
-open Fake 
+open Fake
 
+let buildDir = @"./build/"
+
+/// Delete all non-source files to have a clean build environment.
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+/// The default build target.
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+"Clean"
+    ==> "Default"
 
 Run "Default"
